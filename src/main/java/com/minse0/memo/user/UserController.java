d599afc15bf97fa7fcfbcab067fef91f73f8a0e3
minse0 <3058ms@naver.com>
2025-06-24 16:59:19 +0900

    [김민서] yml 설정 문제 해결

---
+++ src/main/java/com/minse0/memo/user/UserController.java
@@ -0,0 +1,15 @@
+package com.minse0.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	@GetMapping("/join-view")
+	public String joinInput() {
+		return "user/join";
+	}
+}
