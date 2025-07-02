f27677aa5c0927954bfdaf8fba5fda0631be9cad
minse0 <3058ms@naver.com>
2025-07-02 18:17:25 +0900

    no message

--- src/main/java/com/minse0/memo/common/Filemanager.java
+++ src/main/java/com/minse0/memo/common/Filemanager.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class Filemanager {
 	
 	public static final String FILE_UPLOAD_PATH = "C:\\minseo\\upload\\memo";
+	
 	// 파일 저장 기능
 	// 저장된 파일을 클라이언트가 접근할 수 있는 url 경로 return
 	public static String saveFile(long userId, MultipartFile file) {
